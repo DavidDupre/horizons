4df4a8e34a2658715a4c5d9d72c3df2a2a3dc7be
DavidDupre <djdupre7@gmail.com>
2019-06-09 22:20:47 -0700

    Update setup.py for publish to pypi

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='horizons',
-    version='0.1.0',
+    version='0.1.1',
     description='JPL HORIZONS System client',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='David Dupre',
-    license=license,
+    license='MIT License',
     packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Development Status :: 3 - Alpha',
+    ],
 )
