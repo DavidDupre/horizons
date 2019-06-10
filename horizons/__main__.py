e8ddc80d43b6f0514696f8626df0cecb4a5a7115
DavidDupre <djdupre7@gmail.com>
2019-06-09 17:52:33 -0700

    Get vectors

--- horizons/__main__.py
+++ horizons/__main__.py
@@ -1,14 +1,40 @@
-from horizons.core import Horizons
+from horizons import Horizons, BodyResult
+import json
 import logging
 
 
 logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 horizons_log = logging.getLogger(__name__)
-horizons_log.setLevel(logging.DEBUG)
+horizons_log.setLevel(logging.INFO)
 horizons_log.propagate = True
 
 
-with Horizons() as h:
-    major_bodies = h.get_major_bodies()
-    print(major_bodies[:4])
+def get_major_bodies():
+    major_bodies = None
+    with Horizons() as h:
+        major_bodies = h.get_major_bodies()
+    major_bodies_as_dicts = list(map(lambda b: b.__dict__, major_bodies))
+    with open('bodies.json', 'w+') as f:
+        f.write(json.dumps(major_bodies_as_dicts, indent=2))
+
+def get_planet_vectors():
+    planet_ids = [
+        '199',
+        '299',
+        '399',
+        '499',
+        '599',
+        '699',
+        '799',
+        '899',
+    ]
+    vector_map = {}
+    with Horizons() as h:
+        for planet_id in planet_ids:
+            planet_vectors = h.get_vectors(planet_id)
+            vector_map[planet_id] = planet_vectors.__dict__
+    with open('vectors.json', 'w+') as f:
+        f.write(json.dumps(vector_map, indent=2))
+
+get_planet_vectors()
--- horizons/core.py
+++ horizons/core.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from horizons.parse_table import parse_table
 import logging
 import re
@@ -9,6 +10,24 @@ logger = logging.getLogger(__name__)
 
 class HorizonsException(Exception):
     pass
+
+
+@dataclass
+class VectorsResult:
+    """Class for storing the result of a vectors query."""
+    epoch_jd_tdb: float
+    calendar_date_tdb: str
+    pos_km: (float, float, float)
+    vel_kmps: (float, float, float)
+
+
+@dataclass
+class BodyResult:
+    """Class for storing the result of a body query."""
+    id: str
+    name: str
+    designation: str
+    other: str
 
 
 class Horizons:
@@ -38,16 +57,27 @@ class Horizons:
         self.tn.write(command.encode('utf-8'))
         self.tn.write(b'\r\n')
 
-    def _expect(self, expected):
-        expected = str.encode(re.escape(expected))
-        r = self.tn.expect([expected], Horizons.TIMEOUT)
+    def _expect(self, *expected):
+        expected = list(map(re.escape, expected))
+        return self._expect_regex(*expected)
+
+    def _expect_regex(self, *expected):
+        bytes_expected = list(map(str.encode, expected))
+        r = self.tn.expect(bytes_expected, Horizons.TIMEOUT)
+        output = r[2].decode('utf-8')
         if r[1] is None:
-            raise HorizonsException(f'Unexpected output: {r[2]}')
-        return r[2].decode('utf-8')
+            logging.error(f'expected one of: {expected}')
+            logging.error(f'actual:   \"{output}\"')
+            raise HorizonsException(f'Unexpected output: {output}')
+        logger.debug(f'output: {output}')
+        return (r[0], r[1], output)
 
     def get_major_bodies(self):
+        """Get a list of major bodies
+        """
         self._write('MB')
-        output = self._expect('Select ... [F]tp, [M]ail, [R]edisplay, ?, <cr>: ')
+        expect_result = self._expect('Select ... [F]tp, [M]ail, [R]edisplay, ?, <cr>: ')
+        output = expect_result[2]
         lines = output.split('\r\n')
 
         # trim the output to just the table
@@ -55,4 +85,107 @@ class Horizons:
         lines = lines[4:last_line_index]
 
         # parse the output as a table
-        return parse_table(lines)
+        logger.debug('parsing lines as table')
+        table = parse_table(lines)
+        logger.debug('mapping table to BodyResult list')
+        return list(map(
+            lambda item: BodyResult(
+                item['ID#'],
+                item['Name'],
+                item['Designation'],
+                item['IAU/aliases/other'],
+            ),
+            table
+        ))
+
+    def get_vectors(self, body_id: str, center='@sun', ref='eclip', epoch_jd_tdb=2458642.5):
+        """Get position and velocity vectors for a body.
+
+        :param body_id: the body ID as used in Horizons.BodyResult
+        :param center: the ID of a center body (defaults to sun)
+        :param ref: the reference frame (can be eclip, frame, or body; defaults to eclip)
+        :param date_jdtdb: the epoch to find vectors for (defaults to 2458642.5 i.e. June 9, 2019)
+
+        :returns a VectorResult
+        """
+        # We're only interested in the vectors at the provided date, but the API insists we gather points over an
+        # interval. We'll get two sets of vectors at the end but only return the first one (the start date).
+        start_date = f'JD{epoch_jd_tdb}'
+        end_date = f'JD{epoch_jd_tdb + 1}'
+
+        logging.info(f'getting vectors for {body_id} (center: {center}, ref: {ref}, epoch: {epoch_jd_tdb})')
+
+        self._write(body_id)
+        self._expect('Select ... [E]phemeris, [F]tp, [M]ail, [R]edisplay, ?, <cr>: ')
+        self._write('E')
+        self._expect('Observe, Elements, Vectors  [o,e,v,?] : ')
+        self._write('V')
+        expect_result = self._expect(
+            'Coordinate center [ <id>,coord,geo  ] : ',
+            'Use previous center  [ cr=(y), n, ? ] : '
+        )
+        if expect_result[0] == 1:
+            # if the "previous center" option came up, reply No and prepare to be asked which center
+            self._write('N')
+            self._expect('Coordinate center [ <id>,coord,geo  ] : ')
+        self._write(center)
+        self._expect('Reference plane [eclip, frame, body ] : ')
+        self._write(ref)
+        self._expect_regex(r'Starting TDB \[.*\] : ')
+        self._write(start_date)
+        self._expect_regex(r'Ending   TDB \[.*\] : ')
+        self._write(end_date)
+        self._expect('Output interval [ex: 10m, 1h, 1d, ? ] : ')
+        self._write('1d')
+        self._expect('Accept default output [ cr=(y), n, ?] : ')
+        self._write('N')
+        self._expect('Output reference frame [J2000, B1950] : ')
+        self._write('J2000')
+        self._expect('Corrections [ 1=NONE, 2=LT, 3=LT+S ]  : ')
+        self._write('LT')
+        self._expect('Output units [1=KM-S, 2=AU-D, 3=KM-D] : ')
+        self._write('KM-S')
+        self._expect('Spreadsheet CSV format    [ YES, NO ] : ')
+        self._write('YES')
+        self._expect('Output delta-T (TDB-UT)   [ YES, NO ] : ')
+        self._write('NO')
+        self._expect('Select output table type  [ 1-6, ?  ] : ')
+        self._write('2')
+        expect_result = self._expect('>>> Select... [A]gain, [N]ew-case, [F]tp, [M]ail, [R]edisplay, ? : ')
+        output = expect_result[2]
+        self._write('N')
+        self._expect('Horizons> ')
+
+        # find the lines between $$SOE and $$EOE
+        lines = output.split('\r\n')
+        after_soe = False
+        rows = []
+        for line in lines:
+            if line.startswith("$$EOE"):
+                break
+            elif line.startswith("$$SOE"):
+                after_soe = True
+            elif after_soe:
+                rows.append(line)
+
+        # the rows are JDTDB, Calendar Date (TDB), X, Y, Z, VX, VY, VZ
+        results = []
+        for row in rows:
+            items = row.split(',')
+            result = VectorsResult(
+                float(items[0]),
+                items[1].strip(),
+                (
+                    float(items[2]),
+                    float(items[3]),
+                    float(items[4]),
+                ),
+                (
+                    float(items[5]),
+                    float(items[6]),
+                    float(items[7]),
+                ),
+            )
+            results.append(result)
+
+        return results[0]
